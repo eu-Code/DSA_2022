42e9f8198552b59d5047061bd3f9efb6a3242606
Efriem <efidesalew@gmail.com>
2022-03-19 09:32:20 +0300

    Comments added to both Linear.java and Quadratic.java

--- Week_2/Linear.java
+++ Week_2/Linear.java
@@ -6,19 +6,23 @@ import java.io.IOException;
 class Linear{
 	public static void main(String [] args){
 		try {
-		      FileWriter myWriter = new FileWriter("linear.csv");
-		      myWriter.write("n,time\n");
-		      int n=100;
+			// csv file created to store time_elapsed for different values  of n
+		      	FileWriter myWriter = new FileWriter("linear.csv");
+		      	myWriter.write("n,time\n");
+		      	// varying the size of n
 			for(n=1000;n<1000000000;n*=2){
 				long sum =0;	
+				// logging time elapsed for different size of n
 				long start_time = System.nanoTime();
+				// begining of a for loop with linear time complexity
 				for(int i=0;i<n;i++){
 					sum+=i;
 				}
+				// end of a for loop with linear time complexity
 				long end_time = System.nanoTime();
 				myWriter.write(n+","+(end_time-start_time)+"\n");
 			}
-		      myWriter.close();} 
+		      	myWriter.close();} 
 		catch (IOException e) {}
 		
 	}
--- Week_2/Quadratic.java
+++ Week_2/Quadratic.java
@@ -7,21 +7,25 @@ import java.io.IOException;
 class Quadratic{
 	public static void main(String [] args){
 		try {
-		      FileWriter myWriter = new FileWriter("quadratic.csv");
-		      myWriter.write("n,time\n");
-		      int n=100;
+			// Csv file created to store time elpased for different valuse of n
+		      	FileWriter myWriter = new FileWriter("quadratic.csv");
+		      	myWriter.write("n,time\n");
+		      	// varying the size of n
 			for(n=1000;n<1000000;n*=2){
 				long sum =0;	
+				// logging time elapsed
 				long start_time = System.nanoTime();
+				// begining of a nested for loop with quadratic time complexity
 				for(int i=0;i<n;i++){
 					for(int j=0;j<n;j++){
 						sum+=i;
 					}
 				}
+				// end of for loop with quadratic time complexity
 				long end_time = System.nanoTime();
 				myWriter.write(n+","+(end_time-start_time)+"\n");
 			}
-		      myWriter.close();} 
+		      	myWriter.close();} 
 		catch (IOException e) {}
 		
 	}
