4793f49436112195ce4be7fa5fcfa9bbb2e939cc
Efriem <efidesalew@gmail.com>
2022-03-23 17:51:37 +0300

    Adding a node to head and adding a node to the tail of linked list

--- Week_3/TestSLList.java
+++ Week_3/TestSLList.java
@@ -1,12 +1,19 @@
 /*
- * this is test class for our singly
- * linked list impelementation
+ * Test program for our linked list impelementation
+ * Here are some of the operations we are going to test in this program
+ *	1. Checking if the list is empty or not
+ *	2. Checking adding a node to the head of the list
+ *	3. Checking adding a node to the tail of the list
  */
 class TestSLList{
 	public static void main(String[] args){
-		// creating an empty linked list
+		/* Declaring an empty linked list*/
 		SLList linkedList = new SLList();
-		// let's print the result of isEmpty()
-		System.out.println(" Is empty "+ linkedList.isEmpty());
+		/* Let's print the result of isEmpty() */
+		System.out.println("Is empty "+ linkedList.isEmpty());
+		/* Adding a node with info of 5 to the tail of the linked list */
+		linkedList.addToTail(5);
+		/* Checking if the list is still empty */ 
+		System.out.println("Is our list still empty "+linkedList.isEmpty());
 	}
 }
