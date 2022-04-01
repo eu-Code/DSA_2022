838993521c1b5878894db6285cd98b8057fbca20
Efriem <efidesalew@gmail.com>
2022-04-02 02:11:54 +0300

    print, search and delete from head

--- Doubly_Linked_List/DoublyLinkedList.java
+++ Doubly_Linked_List/DoublyLinkedList.java
@@ -3,6 +3,9 @@
  *	1. a method to check if the list is empty of not
  *	2. a method to add a node to the head of doubly linked list
  *	3. a method to add a node to the tail of doubly linked list
+ *	4. a method to print the whole list
+ *	5. a method to search for a node based on info
+ *	6. a method to delete a node from the head of doubly linked list
  */
 class DoublyLinkedList{
 	/* Declare head and tail nodes */
@@ -40,6 +43,7 @@ class DoublyLinkedList{
 	 * 		3. setting the prev of the current head to the address of the new node.
 	 * 		4. updating the head  with the new node.
 	 */
+
 	public void addToHead(int info){
 		/* creating a new node */
 
@@ -57,6 +61,7 @@ class DoublyLinkedList{
 			this.head = this.tail = node;
 		}
 	}
+
 	/* 
 	 * Adding a node to the tail of a doubly linked list
 	 * the steps to add a node to the tail of a doubly linked list are
@@ -68,6 +73,7 @@ class DoublyLinkedList{
 	 * 		3. set the prev of the new node to point to the current tail
 	 * 		4. set the tail to point to the new node
 	 */
+
 	public void addToTail(int info){
 		/* creating a new node */
 		
@@ -85,4 +91,59 @@ class DoublyLinkedList{
 		}
 
 	}
+
+	/*
+	 * Print a list from head to tail
+	 */
+
+	public void printList(){
+		for(Node temp = this.head; temp!=null; temp = temp.next){
+			System.out.println(temp.info);
+		}
+	}
+	
+	/* 
+	 * Search for a node in a doubly linked list based on info
+	 * 	return true if the node exist
+	 * 	return false if the node doesn't exist
+	 */
+
+	public boolean search(int info){
+		for(Node temp = this.head; temp!=null; temp = temp.next){
+			if(temp.info==info){
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/* 
+	 * Deleting a node from the head of doubly linked list
+	 *  if the list is empty do nothing
+	 *
+	 *  if the list is not empty
+	 *  	if the list contains only one node
+	 *  		set both head and tail to null
+	 *  	else if the list contains more than one node
+	 *  		set the head to point to the the next of the current head
+	 *  		set the prev of head to null
+	 */
+
+	public void deleteHead(){
+		/* if the list is not empty */
+		if(!isEmpty()){
+			/* if there is only one node in the list */
+			if(this.head == this.tail){
+				this.head = this.tail = null;
+			}
+			/* if ther are more than one node in the list */
+			else{
+				this.head = this.head.next;
+				this.head.prev = null;
+			}
+		}
+		
+	}
+
 }
--- Doubly_Linked_List/Test.java
+++ Doubly_Linked_List/Test.java
@@ -2,6 +2,10 @@
  * Testing our doubly linked list implemenation
  * 	1. Testing isEmpty method
  * 	2. Testing addToHead method
+ * 	3. Testing addToTail method
+ * 	4. Testing printList method
+ * 	5. Testing search method
+ * 	6. Testing deleteHead method
  */
 class Test{
 	public static void main(String [] args){
@@ -10,9 +14,19 @@ class Test{
 		/* check if the list lis empty or not */
 		System.out.println("Is the list empty: "+list.isEmpty());
 		/* adding a node to the tail of the list */
-		System.out.println("Adding a node with info of 5 to the tail of our doubly linked list");
-		list.addToTail(5);		
-		/* checking if the list is still empty */
-		System.out.println("Is the list still empty: "+list.isEmpty());
+		System.out.println("Adding nodes");
+		list.addToTail(5);
+		list.addToHead(4);
+		list.addToHead(6);
+		list.addToTail(9);
+		/* printing the whole list */
+		list.printList();
+		/* search a node with info of 12 */
+		System.out.println("Is a node with info of 12 in the list: "+list.search(12));
+		/* search a node with info of 9 */
+		System.out.println("Is a node with info of 9 in the list: "+list.search(9));
+		/* deleting head of the list */
+		list.deleteHead();	
+		list.printList();
 	}
 }
