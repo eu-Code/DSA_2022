b4031a021bd99303c0e0224f3d4ca522ef972c5d
Efriem <efidesalew@gmail.com>
2022-04-02 01:08:24 +0300

    adding a node to the head of the list

--- Doubly_Linked_List/DoublyLinkedList.java
+++ Doubly_Linked_List/DoublyLinkedList.java
@@ -1,6 +1,7 @@
 /*
  * Doubly linked list implementation
  *	1. a method to check if the list is empty of not
+ *	2. a method to add a node to the head of doubly linked list
  */
 class DoublyLinkedList{
 	/* Declare head and tail nodes */
@@ -22,5 +23,34 @@ class DoublyLinkedList{
 	public boolean isEmpty(){
 		return this.head == null;
 	}
+	
+	/*
+	 * Adding a node to the head of a doubly linked list,
+	 * the steps to add a node to a head of doubly linked list are
+	 * 	1. creating a new node.
+	 *
+	 * 	if the list is empty
+	 * 		2. update both tail and head to the new node
+	 * 	else if the list is not empty
+	 *
+	 * 		2. setting the next of the new node to the address of the current head.
+	 * 		3. setting the prev of the current head to the address of the new node.
+	 * 		4. updating the head  with the new node.
+	 */
+	public void addToHead(int info){
+		/* creating a new node */
+		
+		Node node = new Node(info);
 
+		/* if the list is not empty */
+		if(!isEmpty()){
+			node.next = this.head;
+			this.head.prev = node;
+			this.head = node;
+		}
+		/* if the list is empty */
+		else{
+			this.head = this.tail = node;
+		}	
+	}
 }
--- Doubly_Linked_List/Test.java
+++ Doubly_Linked_List/Test.java
@@ -1,6 +1,7 @@
 /*
  * Testing our doubly linked list implemenation
  * 	1. Testing isEmpty method
+ * 	2. Testing addToHead method
  */
 class Test{
 	public static void main(String [] args){
@@ -8,5 +9,10 @@ class Test{
 		DoublyLinkedList list = new DoublyLinkedList();
 		/* check if the list lis empty or not */
 		System.out.println("Is the list empty: "+list.isEmpty());
+		/* adding a node to the head of the list */
+		System.out.println("Adding a node with info of 6 to the head of the list");
+		list.addToHead(6);	
+		/* checking if the list is still empty */
+		System.out.println("Is the list still empty: "+list.isEmpty());
 	}
 }
