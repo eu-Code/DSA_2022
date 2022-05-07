4b30f33af30696bf3a7ec39416130a030d804437
Efriem D. Gebie <efidesalew@gmail.com>
2022-05-07 22:46:30 +0300

    minor restructuring

--- Binary_Search_Tree/BST.java
+++ Binary_Search_Tree/BST.java
@@ -1,7 +1,6 @@
 /*
  * Binary search tree impelementation
  * 	1. Checking if a given BST is empty or not
- * 	2. Inserting a node into a BST
  */
 public class BST{
 	/*
@@ -19,40 +18,6 @@ public class BST{
 	 */
 	public boolean isEmpty(){
 		return this.root == null;
-	}
-	/*
-	 * Inserting a node into a binary search tree, while inserting a node into a binary search tree we are required to consider to cases
-	 * 	Case I: adding a node to an empty BST, in this case it is just creating a new BST node assingning a new created node as a root node
-	 * 	Case II: adding a node to a non empty bST, here we are required to traverse through the tree and find a right location to attach this new node
-	*/
-	public void insert(int key){
-		/*
-		 * adding a node to an empty binary search tree
-		 */
-		if(this.root == null){
-			this.root = new BSTNode(key);
-		}
-		/*
-		 * adding a node to a non-empty binary search tree
-		 */
-		else{
-			/* 
-			 * finding a right location for the new node
-			 */
-			BSTNode p = root;
-			BSTNode prev;
-			while(p!=null){
-				prev = p;
-				if(p.key<key) p = p.right;
-				else p = p.left;
-			}
-			/*
-			 * attaching the new node to the tree
-			 */
-			if(prev.key<key) prev.right = new BSTNode(key);
-			else prev.left = new BSTNode(key);
-
-		}
 	}
 
 }
